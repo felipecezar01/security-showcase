91c8f62ba9633d27334f7c7e147d94a33b0606d1
Felipe Cézar <felipecezarcc@gmail.com>
2024-10-30 09:01:45 -0300

    Implementa rota de comentários segura contra XSS

--- routes/comment.js
+++ routes/comment.js
@@ -1,9 +1,12 @@
 // routes/comment.js
 const express = require('express');
 const router = express.Router();
-const { addCommentVulneravel } = require('../controllers/commentController');
+const { addCommentVulneravel, addCommentSeguro } = require('../controllers/commentController');
 
 // Rota para adicionar um comentário vulnerável a XSS
 router.post('/comment-vulneravel', addCommentVulneravel);
+
+// Rota para adicionar um comentário seguro contra XSS
+router.post('/comment-seguro', addCommentSeguro);
 
 module.exports = router;
