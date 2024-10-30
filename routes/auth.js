089ecbe536a9ae11ec276115edce0469397604dc
Felipe Cézar <felipecezarcc@gmail.com>
2024-10-30 08:48:57 -0300

    Implementa rota de login segura contra SQL Injection

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 // routes/auth.js
 const express = require('express');
 const router = express.Router();
-const { loginVulneravel } = require('../controllers/authController');
+const { loginVulneravel, loginSeguro } = require('../controllers/authController');
 
 router.post('/login-vulneravel', loginVulneravel);
+router.post('/login-seguro', loginSeguro); // Nova rota para o login seguro
 
 module.exports = router;
